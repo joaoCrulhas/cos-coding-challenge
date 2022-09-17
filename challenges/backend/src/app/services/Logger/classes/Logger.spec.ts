de073318de017b941fa80e5f32ae5736b18e3227
joaocrulhas <joao@isarlend.com>
2022-09-17 18:45:42 +0200

    AddLoggerToError

--- challenges/backend/src/app/services/Logger/classes/Logger.spec.ts
+++ challenges/backend/src/app/services/Logger/classes/Logger.spec.ts
@@ -8,7 +8,7 @@ const makeSut = (): {
   return { sut };
 };
 describe("LoggerService", () => {
-  it("Should call the console.debug() with correct message", async () => {
+  it("Should call the console.debug() with correct message", () => {
     const spy = Sinon.spy(console, "debug");
     const { sut } = makeSut();
     const message = "test";
@@ -16,13 +16,22 @@ describe("LoggerService", () => {
     Sinon.assert.calledOnce(spy);
     Sinon.assert.calledWith(spy, `[DEBUG]: ${message}`);
   });
-  it("Should call debug with params if params is provided", async () => {
+  it("Should call debug with params if params is provided", () => {
     const spy = Sinon.spy(console, "debug");
     const { sut } = makeSut();
     const message = "test";
     const params = "paramsTEs";
     sut.debug(message, params);
     Sinon.assert.calledWith(spy, "[DEBUG]: test, params paramsTEs");
+  });
+  it("Should call error with correct arguments", () => {
+    const spy = Sinon.spy(console, "error");
+    const { sut } = makeSut();
+    const message = "test";
+    const params = "paramsTEs";
+    sut.error(message, params);
+    Sinon.assert.calledOnce(spy);
+    Sinon.assert.calledWith(spy, "[ERROR]: test, stackTrace paramsTEs");
   });
   afterEach(() => {
     Sinon.restore();
