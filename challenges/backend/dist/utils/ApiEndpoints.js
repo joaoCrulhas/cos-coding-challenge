2c345aab5af1caffb55ec2341cb4bd64603f4486
joaocrulhas <joao@isarlend.com>
2022-09-18 20:50:10 +0200

    fix

--- challenges/backend/dist/utils/ApiEndpoints.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.API_ENDPOINTS = void 0;
-const API_ENDPOINTS = {
-    TOKEN_GENERATOR: "api/v1/authentication",
-    AUCTION_BIDDING: "api/v1/auction/salesman/{customerId}/_all/bidding-data",
-};
-exports.API_ENDPOINTS = API_ENDPOINTS;
--- challenges/backend/dist/utils/auctions/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.calculateRatioForEachAuction = exports.averageBidsPerAuction = void 0;
-const AverageBidsPerAuction_1 = require("./AverageBidsPerAuction");
-Object.defineProperty(exports, "averageBidsPerAuction", { enumerable: true, get: function () { return AverageBidsPerAuction_1.averageBidsPerAuction; } });
-const CalculateRatioForAuction_1 = require("./CalculateRatioForAuction");
-Object.defineProperty(exports, "calculateRatioForEachAuction", { enumerable: true, get: function () { return CalculateRatioForAuction_1.calculateRatioForEachAuction; } });
