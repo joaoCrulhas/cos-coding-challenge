629b7582435debab2c2912bb65525a3482759b5b
joaocrulhas <joao@isarlend.com>
2022-09-18 21:00:51 +0200

    docker file changes

--- challenges/backend/src/app/utils/ApiEndpoints.ts
+++ challenges/backend/src/app/utils/ApiEndpoints.ts
@@ -1,4 +1,5 @@
 const API_ENDPOINTS = {
+  BASE_URL: "https://api-core-dev.caronsale.de",
   TOKEN_GENERATOR: "api/v1/authentication",
   AUCTION_BIDDING: "api/v1/auction/salesman/{customerId}/_all/bidding-data",
 };
